65d9ec244f4877a74972f72189a8d9f9f62ac198
Gabriel Thome <eugabrielthome@gmail.com>
2023-12-09 22:33:44 -0300

    add speedInsights

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 document.title = 'X-Ticket';
 
 ReactDOM.render(
   <React.StrictMode>
+    <SpeedInsights />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
